9691ad94a1b6f1167e9ce302a8c5fa785f7511de
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 02:24:59 +0700

    feat: Pass sumAll
    Use destructuring assignment to swap max, min and use typeof to check for number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+function isInvalid(x) {
+    if ((typeof x) !== "number") return true;
+    if (x < 0) return true;
+    return false;
+}
 
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (isInvalid(a) || isInvalid(b)) return "ERROR";
+    [a, b] = (a > b) ? [b, a] : [a, b];
+    for (i = a; i <= b; i++) sum += i;
+    return sum;
+    // 
 };
 
 // Do not edit below this line
