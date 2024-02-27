54b2ee4b56e8b80ee23c0089b7cfcd19d6f52b55
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 02:39:41 +0700

    feat: Pass leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const divisibleBy4 = (num) => (num % 4 == 0);
+const divisibleBy100 = (num) => (num % 100 == 0);
+const divisibleBy400 = (num) => (num % 400 == 0);
 
+const leapYears = function(year) {
+    // Leap years are divisible by four. 
+    // However, years divisible by 100 are not leap years
+    // unless they are divisible by 400
+    if (divisibleBy4(year)) {
+        if (divisibleBy100(year))
+            if (divisibleBy400(year)) return true;
+            else return false;
+        else return true;
+    } return false;
 };
 
 // Do not edit below this line
