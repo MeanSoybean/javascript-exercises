6c3c2dc4862edf3d00b9391869cc5e348c3450aa
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-27 16:05:27 +0700

    feat: Pass repeatString
    Add guard clauses for exceptional cases (0, 1, negative), otherwise use String.prototype.repeat

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    if (amount == 0) return "";
+    if (amount == 1) return string;
+    if (amount < 0) return "ERROR";
+    return string.repeat(amount);
 };
 
 // Do not edit below this line
