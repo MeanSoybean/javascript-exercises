c8d283c8f5e2bae0b13733d88596e61ed0a285eb
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 02:04:24 +0700

    feat: Pass removeFromArray
    Use Array.prototype.includes() and rest parameters, and .forEach() to add to new memory

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, ...elementsToRemove) {
+    outputArray = [];
+    array.forEach((value) => {
+        if (!(elementsToRemove.includes(value))) {
+            outputArray.push(value);
+        }
+    })
+    return outputArray;
 };
 
 // Do not edit below this line
