dbe6953f15b4c03f4eba2b0bee47b82aa96a926f
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 02:52:55 +0700

    feat: Pass tempConversion
    Nice trick to shift decimal place then round then shift it back with the roundToOneDecimalPlace function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundToOneDecimalPlace = function(num) {
+  return Math.round(num*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(f) {
+  return roundToOneDecimalPlace((f-32)/1.8);
+};
+
+const convertToFahrenheit = function(c) {
+  return roundToOneDecimalPlace(1.8*c+32);
 };
 
 // Do not edit below this line
