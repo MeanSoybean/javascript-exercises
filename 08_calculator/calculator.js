671b34b708619b8e1b3ef2a3f4a1a356c8798e1f
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 16:18:08 +0700

    feat: Pass calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	let result = 0;
+  myArray.forEach(
+    (value) => {
+      result += value;
+    }
+  )
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(myArray) {
+  let result = 1;
+  myArray.forEach(
+    (value) => {
+      result *= value;
+    }
+  )
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  let result = 1;
+  for (i = 1; i <= a; i++) result *= i;
+  return result;
 };
 
 // Do not edit below this line
