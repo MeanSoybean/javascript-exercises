2f1b4a23633888f14ace462951d5daec83caf124
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 16:49:53 +0700

    feat: Pass palindromes
    Use regex to capture only alphanumeric, then compare forward with backward

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(s) {
+    const regex = /[a-z0-9]/gi;
+    const alphaOnly = s.match(regex);
+    const forward = alphaOnly.join('').toLowerCase();
+    const backward = alphaOnly.toReversed().join('').toLowerCase();
+    return forward == backward;
 };
 
 // Do not edit below this line
