01952ea6964cb355fe29e87f961eb63a5ad4d58c
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 16:55:14 +0700

    feat: Pass fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = Number.parseInt(x);
+    if (x < 0) return "OOPS";
+    return fib(x);
 };
 
+const fib = function(x) {
+    if (x == 0) return 0;
+    if (x == 1 || x == 2) return 1;
+    return fib(x-1) + fib(x-2);
+}
 // Do not edit below this line
 module.exports = fibonacci;
