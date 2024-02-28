42005e220981719782f3e8dcd851f9dfe1d11d21
Stan <47215667+MeanSoybean@users.noreply.github.com>
2024-02-28 22:11:24 +0700

    feat: Pass findTheOldest
    Learn about how to use reduce and Date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    return (person.yearOfDeath || (new Date()).getFullYear()) - person.yearOfBirth
+} 
 
+const findTheOldest = function(people) {
+    return people.reduce((lastOldestPerson, thisPerson) => {
+        if (!lastOldestPerson) return thisPerson;
+        if (getAge(lastOldestPerson) > getAge(thisPerson)) return lastOldestPerson;
+        return thisPerson;
+    })
 };
 
 // Do not edit below this line
